bcd14ffb7d4266e153dc9f701c0d67c42ca48230
Asilbek <Leonlloyd47@gmail.com>
2026-01-24 23:44:57 +0900

    creating first Rest API endpoint with CRUD

---
+++ expenses/views.py
@@ -0,0 +1,43 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Expense
+from .serializers import ExpenseSerializer
+
+
+@api_view(['GET', 'POST'])
+def expense_list(request):
+    if request.method == 'GET':
+        expenses = Expense.objects.all()
+        serializer = ExpenseSerializer(expenses, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = ExpenseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+
+@api_view(['GET', 'PUT', 'DELETE'])      
+def expense_detail(request, id):
+    try:
+        expense = Expense.objects.get(id=id)
+    except Expense.DoesNotExist:
+        return Response({'error': 'Expense not found'}, status=404)
+    
+    if request.method == 'GET':
+        serializer = ExpenseSerializer(expense)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        serializer = ExpenseSerializer(expense, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    if request.method == 'DELETE':
+        expense.delete()
+        return Response(status=204)
+
