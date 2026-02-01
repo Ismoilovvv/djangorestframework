95d39a00d5447ac19a36116ed1e8466cae5b882f
Asilbek <Leonlloyd47@gmail.com>
2026-02-01 21:28:21 +0900

    applying authentication

--- tracker_project/urls.py
+++ tracker_project/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include('expenses.urls')),
     path('api/', include('tasks.urls')),
 ]
