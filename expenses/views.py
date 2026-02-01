01e341253ee4c76a7e4d91bd0150888518ba40f0
Asilbek <Leonlloyd47@gmail.com>
2026-02-01 17:37:48 +0900

    converting CBV to generic views

--- expenses/views.py
+++ expenses/views.py
@@ -1,49 +1,13 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics 
 from .models import Expense
 from .serializers import ExpenseSerializer
 
-class ExpenseList(APIView):
-    def get(self, request):
-        expenses = Expense.objects.all()
-        serializer = ExpenseSerializer(expenses, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = ExpenseSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-class ExpenseDetail(APIView):
-    def get_object(self, id):
-        try:
-            return Expense.objects.get(id=id)
-        except Expense.DoesNotExist:
-            return None
-        
-    def get(self, request, id):
-        expense = self.get_object(id)
-        if expense is None:
-            return Response({'error': 'Expense not found!'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = ExpenseSerializer(expense)
-        return Response(serializer.data)
-    
-    def put(self, request, id):
-        expense = self.get_object(id)
-        if expense is None:
-            return Response({'error': 'Expense not found!'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = ExpenseSerializer(expense, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=400)
-        
-    def delete(self, id):
-        expense = self.get_object(id)
-        if expense is None:
-            return Response({'error': 'Expense not found!'}, status=status.HTTP_404_NOT_FOUND)
-        expense.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ExpenseList(generics.ListCreateAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+    lookup_field = 'id'
+
+class ExpenseDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+    lookup_field = 'id'
--- tasks/views.py
+++ tasks/views.py
@@ -1,49 +1,13 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
 
-class TaskList(APIView):
-    def get(self, request):
-        tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = TaskSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()  
-            return Response(serializer.errors, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-class TaskDetail(APIView):
-    def get_object(self, id):
-        try:
-            return Task.objects.get(id=id)
-        except Task.DoesNotExist:
-            return None
-        
-    def get(self, request, id):
-        task = self.get_object(id)
-        if task is None:
-            return Response({'error': 'Task not found'}, status=404)
-        serializer = TaskSerializer(task)
-        return Response(serializer.data)
-    
-    def put(self, request, id):
-        task = self.get_object(id)
-        if task is None:
-            return Response({'error': 'Task not found'}, status=404)
-        serializer = TaskSerializer(task, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
-    
-    def delete(self, request, id):
-        task = self.get_object(id)
-        if task is None:
-            return Response({'error': 'Task not found!'}, status=404)
-        task.delete()
-        return Response(status=204)
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = 'id'
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = 'id'
