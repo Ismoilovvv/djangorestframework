c8683b29cdf399168ac572c79f381837c3d1b1fc
Asilbek <Leonlloyd47@gmail.com>
2026-02-07 00:06:16 +0900

    adding user

--- expenses/models.py
+++ expenses/models.py
@@ -1,9 +1,9 @@
 from django.db import models 
-from django.conf import settings 
+from django.contrib.auth.models import User 
 
 class Expense(models.Model):
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        User,
         on_delete=models.CASCADE,
         related_name='expenses'
     )
